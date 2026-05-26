c0e112ef2433e4b033d52fdeb64a2f66d53fc384
Summer cold <110237230+CRri31415@users.noreply.github.com>
2026-05-26 20:13:22 +0900

    Update kanthal.c

--- src/kanthal.c
+++ src/kanthal.c
@@ -1,28 +1,27 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include "kanthal.h"
 
-
-
-arena* arena_init(uint32_t ani_size)
+arena* arena_init(uintptr_t ani_size)
     { arena *ani_ptr=malloc(sizeof(arena))
     ; if(ani_ptr==NULL)
         { error_raise(ERROR_ARENA_INIT)
         ; return NULL
         ; }
-      (*ani_ptr).space=malloc(ani_size+KANTHAL_CACHELINE_SIZE*2)
+      (*ani_ptr).space=malloc(ani_size+KANTHAL_CACHELINE_SIZE)
     ; if((*ani_ptr).space==NULL)
         { free(ani_ptr)
         ; error_raise(ERROR_ARENA_INIT)
         ; return NULL
         ; }
-      (*ani_ptr).space[(KANTHAL_CACHELINE_SIZE*2-((*ani_ptr).space&KANTHAL_CACHELINE_SIZE))-1]=(uint8_t)(KANTHAL_CACHELINE_SIZE*2-((*ani_ptr).space&KANTHAL_CACHELINE_SIZE))
-    ; (*ani_ptr).space+=(KANTHAL_CACHELINE_SIZE*2-((*ani_ptr).space&KANTHAL_CACHELINE_SIZE))
+      uintptr_t ani_offset=KANTHAL_CACHELINE_SIZE-(((uintptr_t)(*ani_ptr).space)&(KANTHAL_CACHELINE_SIZE-1))
+    ; (*ani_ptr).space[ani_offset-1]=(uint8_t)ani_offset
+    ; (*ani_ptr).space+=ani_offset
     ; (*ani_ptr).size_limit=ani_size-(*ani_ptr).space[-1]
     ; (*ani_ptr).hot_offset=0
     ; (*ani_ptr).cold_offset=(*ani_ptr).size_limit
     ; return ani_ptr
     ; }
 
-uint32_t arena_alloc(arena* aac_arena,uint8_t aac_datatype,uint8_t aac_datasize)
+uintptr_t arena_alloc(arena* aac_arena,uint8_t aac_datatype,uint8_t aac_datasize)
     { if (*aac_arena)/
     }
